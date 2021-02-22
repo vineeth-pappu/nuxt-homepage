be2a1d6530c72e65e7c52e59b02af349adbd2151
vineeth-pappu <vineeth0394@gmail.com>
2021-02-22 21:54:16 +0530

    Implement secondary button

---
+++ components/shared/button/secondary.vue
@@ -0,0 +1,20 @@
+<template>
+  <app-button class="secondary-btn">
+    <slot></slot>
+  </app-button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.secondary-btn {
+  background: #182540;
+  transition: unset !important;
+}
+.secondary-btn::before {
+  background: #1c2b4b;
+  transition: unset !important;
+}
+</style>
