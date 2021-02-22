415dcc1317fe0c857d7a443c1efe373e648d5926
vineeth-pappu <vineeth0394@gmail.com>
2021-02-22 21:53:21 +0530

    Implement Button component
     - primary & secondary buttons

--- components/shared/button/index.vue
+++ components/shared/button/index.vue
@@ -1,11 +1,42 @@
 <template>
-  <div>
-    <h1>Button</h1>
-  </div>
+  <button class="btn app-btn">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.app-btn {
+  position: relative;
+  /* background: linear-gradient(90deg, #e50612, #730309); */
+  font-size: 1.125rem;
+  color: #fff;
+  outline: none;
+  border: none;
+  box-shadow: none;
+  border-radius: 50px;
+  padding: 1.25rem 3.75rem;
+  text-transform: capitalize;
+  transition: all 0.5s linear;
+  overflow: hidden;
+  z-index: 1;
+}
+.app-btn::before {
+  position: absolute;
+  content: '';
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  /* background: linear-gradient(90deg, #730309, #e50612); */
+  z-index: -1;
+  opacity: 0;
+  transition: all 0.5s linear;
+}
+.app-btn:hover::before {
+  opacity: 1;
+}
+</style>
---
+++ components/shared/button/primary.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-button class="primary-btn">
+    <slot></slot>
+  </app-button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.primary-btn {
+  background: linear-gradient(90deg, #e50612, #730309);
+}
+.primary-btn::before {
+  background: linear-gradient(90deg, #730309, #e50612);
+}
+</style>
